a6eabda5f1ac8d06bdcf5d4c1e4f2a552d77d697
Saad Muhammad Bin Ramzan <184134144+saadkhan86@users.noreply.github.com>
2026-08-13 20:55:54 +0500

    Add health check endpoint to server

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,10 @@ app.use(helmet())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: "10mb" }))
-
+app.get("/",(req: Request,res: Response)=>
+    {
+        res.status(200).json({success:true,message:"server is working"})
+    })
 app.use("/api/v1", router)
 
 
